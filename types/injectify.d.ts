3b3b5c2693168d6b80ef4dfdcc6f79cc0407100b
Yoshihiro OKUMURA <orrisroot@gmail.com>
2023-08-21 18:12:04 +0900

    removed build result files from git.

--- types/injectify.d.ts
+++
@@ -1,5 +0,0 @@
-import type { LoaderContext } from 'webpack';
-import type { InputSourceMap } from './types';
-declare const injectify: (context: LoaderContext<object>, source: string, inputSourceMap?: InputSourceMap) => import("@babel/core").BabelFileResult | null;
-export default injectify;
-//# sourceMappingURL=injectify.d.ts.map
--- types/wrapper_template.d.ts
+++
@@ -1,3 +0,0 @@
-declare const _default: (arg?: import("@babel/template").PublicReplacements | undefined) => import("@babel/types").Statement | import("@babel/types").Statement[];
-export default _default;
-//# sourceMappingURL=wrapper_template.d.ts.map
